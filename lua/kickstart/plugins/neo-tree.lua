b6136ee7c3c010e2c815ee216f4f03975afaa447
decki-id <d.herdiawan.s@gmail.com>
2026-05-15 20:44:52 +0700

    Revert the keys to 'reveal'

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -13,7 +13,7 @@ return {
   },
   lazy = false,
   keys = {
-    { '\\', ':Neotree toggle<CR>', desc = 'NeoTree toggle', silent = true },
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   ---@module 'neo-tree'
   ---@type neotree.Config
