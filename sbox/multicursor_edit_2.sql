bce894cf1801003be4e4a3be130c030664fffc34
decki-id <d.herdiawan.s@gmail.com>
2026-08-03 15:15:09 +0700

    Add sandbox files for practice

---
+++ sbox/multicursor_edit_2.sql
@@ -0,0 +1,9 @@
+member_profile_id
+shopify_customer_id varchar default '-'::character varying not null,
+dept_code varchar default '-'::character varying not null,
+trans_no varchar default '-'::character varying not null,
+order_no varchar default '-'::character varying not null,
+trans_name varchar default 'ONLINE'::character varying not null,
+channel_name varchar default 'WEBSITE'::character varying not null,
+order_status varchar default '-'::character varying not null,
+barcode varchar default '-'::character varying not null,
